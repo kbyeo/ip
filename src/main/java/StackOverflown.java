22ad5bda389d3a68b7e31d99428a0ab2b65285ef
kbyeo <kbyeo126555@gmail.com>
2025-08-19 21:27:13 +0800

    Add personality to StackOverflown messages

--- src/main/java/StackOverflown.java
+++ src/main/java/StackOverflown.java
@@ -2,9 +2,9 @@ public class StackOverflown {
     public static void main(String[] args) {
         String lineSeparation = "____________________________________________________________";
         String botName = "StackOverflown";
-        String exitLine = "Bye. Hope to see you again soon!";
-        String message = String.format("%s\n Hello! I'm %s!\n What can I do for you?\n%s\n %s\n%s",
-                lineSeparation, botName, lineSeparation, exitLine, lineSeparation);
+        String exitLine = "Aww, you’re leaving already? It’s been such a \n pleasure, can’t wait till next time! :)";
+        String message = String.format("%s\n Hey! %s here, thrilled to see you!\n Let's dive RIGHT in, what can I " +
+                "do for you? :)\n%s\n %s\n%s", lineSeparation, botName, lineSeparation, exitLine, lineSeparation);
         System.out.println(message);
     }
 }
